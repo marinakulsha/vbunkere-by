f50a90248d9bc1c5c915c8f0f7002ec09ab0097d
Марина Кульша <marinakulsa@Marinas-MacBook-Pro.local>
2019-10-19 22:40:01 +0300

    update

--- src/js/gallery.js
+++ src/js/gallery.js
@@ -4,16 +4,16 @@ var iconMore = document.querySelector('.arrow-down');
 var seeLess = document.getElementById('watchLess');
 var iconLess = document.querySelector('.arrow-up');
 
-seeMore.addEventListener('click', function() {
-    aditionalSection.classList.add('show_section');
-});
-iconMore.addEventListener('click', function() {
-    aditionalSection.classList.add('show_section');
-});
+// seeMore.addEventListener('click', function() {
+//     aditionalSection.classList.add('show_section');
+// });
+// iconMore.addEventListener('click', function() {
+//     aditionalSection.classList.add('show_section');
+// });
 
-seeLess.addEventListener('click', function() {
-    aditionalSection.classList.remove('show_section');
-});
-iconLess.addEventListener('click', function() {
-    aditionalSection.classList.remove('show_section');
-});
+// seeLess.addEventListener('click', function() {
+//     aditionalSection.classList.remove('show_section');
+// });
+// iconLess.addEventListener('click', function() {
+//     aditionalSection.classList.remove('show_section');
+// });
